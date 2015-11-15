3e28c736dacebef650a3ae0ee14a3c5467040849
Joas1988 <Joas1988@gmail.com>
2015-11-16 00:38:56 +0100

    Update config.rb

--- servers/unicorn/config.rb
+++ servers/unicorn/config.rb
@@ -1,6 +1,8 @@
+worker_processes 1
+
 listen "#{ENV['OPENSHIFT_RUBY_IP']}:#{ENV['OPENSHIFT_RUBY_PORT']}"
-pid "#{ENV['OPENSHIFT_RUBY_SERVER_PID']}"
-timeout 30
+
+pid "#{ENV['OPENSHIFT_RUBY_DIR']}/run/server.pid"
+
 stdout_path "#{ENV['OPENSHIFT_RUBY_LOG_DIR']}/unicorn_stdout.log"
 stderr_path "#{ENV['OPENSHIFT_RUBY_LOG_DIR']}/unicorn_stderr.log"
-
